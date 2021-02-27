4a2836724a2436281f75a7660983c2ea3794f69d
KamilBienias <kamil.bienias.math@gmail.com>
2021-02-27 18:30:53 +0100

    wstep do wyrazen regularnych

---
+++ 02_word_count/02_regex.py
@@ -0,0 +1,15 @@
+import re
+
+# takiego typu wyrazenie szukamy
+# W nawiasie jest wzor czyli pattern.
+# Z przodu r oznacza ze jest to wyrazenie regularne
+# \w to word character, czyli male i duze litery, cyfry oraz podkreslnik
+# Plus na koncu oznacza co najmniej 1 element.
+# Chcemy przynajmniej jednoliterowe wyrazy
+WORD_RE = re.compile(r'[\w]+')
+
+# uzywanie patternu
+words = WORD_RE.findall('Big data, hadoop and map reduce. (hello world!)')
+print(words)
+# zwroci liste slow:
+# ['Big', 'data', 'hadoop', 'and', 'map', 'reduce', 'hello', 'world']
