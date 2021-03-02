e50feac73a313b6d3eca992db731af2f36d8d9e2
KamilBienias <kamil.bienias.math@gmail.com>
2021-03-02 13:03:00 +0100

    wykres opoznien dla kazdej z airline

---
+++ 04_airports/average_dep_arr_delay_by_airline.py
@@ -0,0 +1,46 @@
+# Etap9. Odcinek: JupyterNotebook: Eksploracja wynikow (Plotly) drugi
+
+# oryginalnie bylo w jupyter notebook
+
+import pandas as pd
+
+# seperatorem jest tabulator
+# to_split jest kolumna, ktora trzeba podzielic na dwie kolumny.
+# Bez names= to wtedy pierwszy wiersz odczytuje jako naglowek tabeli
+df = pd.read_csv('/home/dell/PycharmProjects/big-data-hadoop-map-reduce/04_airports/average_delay_by_airline.csv',
+                 sep='\t',
+                 names=['airline', 'to_split'])
+# wyciecie [1:-1] pozbywa sie nawiasow kwadratowych.
+# Robi z jednej kolumny dwie Series
+df[['dep_delay', 'arr_delay']] = df.to_split.str[1:-1].str.split(', ').apply(pd.Series)
+# usuwa stara kolumne
+df = df.drop(columns='to_split')
+print(df)
+
+import plotly.express as px
+
+# czemu zle slupki pokazuje ?
+fig = px.bar(df, x='airline', y='dep_delay', orientation='v', color='airline')
+fig.show()
+
+fig = px.bar(df, x='airline', y='arr_delay', orientation='v', color='airline')
+fig.show()
+
+import plotly.graph_objects as go
+
+# jeden slupek obok drugiego
+fig = go.Figure(data=[
+                     # pierwszy slad
+                     go.Bar(x=df.airline, y=df.dep_delay, name='departure delay', marker_color='#517CAA'),
+                     # drugi slad
+                     go.Bar(x=df.airline, y=df.arr_delay, name='arrival delay', marker_color='#E09F1F')
+                     ],
+               layout=go.Layout(title='Average Departure and Arrival Delay by Airline USA 2015'))
+fig.show()
+
+# jeden slupek na drugim
+fig = go.Figure(data=[go.Bar(x=df.airline, y=df.dep_delay, name='departure delay', marker_color='#517CAA'),
+                     go.Bar(x=df.airline, y=df.arr_delay, name='arrival delay', marker_color='#E09F1F')],
+               layout=go.Layout(title='Average Departure and Arrival Delay by Airline USA 2015-01',
+                               barmode='stack'))
+fig.show()
