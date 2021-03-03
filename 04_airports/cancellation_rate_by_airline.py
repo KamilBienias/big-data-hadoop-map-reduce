5df2ca93fadca159d68e50026a76f6dda3777c21
KamilBienias <kamil.bienias.math@gmail.com>
2021-03-03 11:03:15 +0100

    wykres opoznien dla kazdej z airline

---
+++ 04_airports/cancellation_rate_by_airline.py
@@ -0,0 +1,16 @@
+# Etap9. Odcinek: Rozwiazanie: JupyterNotebook: Eksploracja wynikow (Protly) trzeci
+
+import pandas as pd
+
+# cr to cancellation rate
+df = pd.read_csv('cancellation_rate.csv', sep='\t', names=['airline', 'cr'])
+print(df)
+
+import plotly.express as px
+
+px.bar(df, x='airline', y='cr', color='airline')
+
+import plotly.graph_objects as go
+# wykres kolowy, hole to dziura w srodku
+fig = go.Figure(data=go.Pie(labels=df.airline, values=df.cr, hole=0.5))
+fig.show()
