0345fba009a4ff1309421e64c639626b29048db6
KamilBienias <kamil.bienias.math@gmail.com>
2021-03-04 09:25:09 +0100

    Rozklad zmiennej Score

---
+++ 06_food_reviews/02_score_distribution.py
@@ -0,0 +1,22 @@
+# Etap13. Odcinek: Rozgrzewka - rozklad ocen
+
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+
+# uruchomienie na testowym:
+# python3 02_score_distribution.py test.txt
+# uruchomienie na calym zbiorze:
+# python3 02_score_distribution.py prep_reviews.txt
+class MRFood(MRJob):
+
+    def mapper(self, _, line):
+        (Id, ProductId, UserId, ProfileName, HelpfulnessNumerator, HelpfulnessDenominator,
+         Score, Time, Summary, Text) = line.split('\t')
+        yield Score, 1
+
+    def reducer(self, key, values):
+        yield key, sum(values)
+
+
+if __name__ == '__main__':
+    MRFood.run()
